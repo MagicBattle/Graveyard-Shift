57e73720d5f4690f179f7fdbaddb46641ee77b55
HuanSwallo <tannernguyen561@gmail.com>
2025-11-19 23:57:13 -0800

    added new audio

--- -graveyard-shift/scripts/death.gd
+++ -graveyard-shift/scripts/death.gd
@@ -1,29 +1,26 @@
 extends Node3D
 
-
 @onready var anim: AnimationPlayer = $Camera3D/CanvasLayer/AnimationPlayer
 @onready var death_screen_ui = $Camera3D/CanvasLayer/DeathScreen
-@onready var audio_player = $Camera3D/CanvasLayer/AudioStreamPlayer
+
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	anim.process_mode = Node.PROCESS_MODE_ALWAYS
-	audio_player.process_mode = Node.PROCESS_MODE_ALWAYS
 	death_screen_ui.process_mode = Node.PROCESS_MODE_ALWAYS
 
 	death_screen_ui.visible = false
 	
 	anim.play("blur")
-	audio_player.play()
 	anim.animation_finished.connect(_on_animation_end)
-
+	
 
 func _on_animation_end(anim_name: String) -> void:
 	if anim_name == "blur":
-		audio_player.stop()
 		death_screen_ui.visible = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		anim.play("fade_in")
+
 
 func _on_continue_pressed() -> void:
 	GameManager.start_game()  # REPLACE WITH LOAD CHECKPOINT
