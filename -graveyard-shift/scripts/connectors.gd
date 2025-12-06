68c50f4128901ac5397819672f023a92a4b8826a
get-jumped <yamada_ak@icloud.com>
2025-12-06 15:34:12 -0800

    fixing main big connector

--- -graveyard-shift/scripts/connectors.gd
+++ -graveyard-shift/scripts/connectors.gd
@@ -1,20 +1,37 @@
 extends NavigationRegion3D
 
 var nav_region : NavigationRegion3D
+var open : NavigationRegion3D
+var close : NavigationRegion3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	NoiseManager.door_change.connect(rebake)
 	nav_region = $"."
+	#open = $"../Open"
+	#close = $"../Close"
 
 
 func rebake(mesh : String):
 	if nav_region.name.to_lower() == mesh:
+		#Closing
 		if nav_region.get_navigation_layers() == 1:
 			nav_region.set_navigation_layer_value(4, true)
 			nav_region.set_navigation_layer_value(1, false)
+			
+			#if nav_region.name.to_lower() == "maindoor":
+				#open.set_navigation_layer_value(4, true)
+				#open.set_navigation_layer_value(1, false)
+				#close.set_navigation_layer_value(4, false)
+				#close.set_navigation_layer_value(1, true)
+		
+		#Opening
 		elif nav_region.get_navigation_layers() == 8:
-			if mesh == "balloondoor":
-				print("NEW SUCCESS")
 			nav_region.set_navigation_layer_value(4, false)
 			nav_region.set_navigation_layer_value(1, true)
+			
+			#if nav_region.name.to_lower() == "maindoor":
+				#open.set_navigation_layer_value(4, false)
+				#open.set_navigation_layer_value(1, true)
+				#close.set_navigation_layer_value(4, true)
+				#close.set_navigation_layer_value(1, false)
--- -graveyard-shift/scripts/maze_room.gd
+++ -graveyard-shift/scripts/maze_room.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 var inside_maze : bool = false
 
+
 func _on_maze_trigger_body_entered(body: Node3D) -> void:
 	if not inside_maze:
 		if body is CharacterBody3D:
