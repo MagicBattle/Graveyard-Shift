49a319c158346a5de9aba0e3137266ce1217a965
ArnocoLan <lmarnoco@ucdavis.edu>
2025-12-07 20:28:12 -0800

    Added Manager Logic (Dhruv)
    - Added Phase Checks to all stuff (Dhruv)
    - Fixed Menu thing (Dhruv)

--- -graveyard-shift/scripts/maze_room.gd
+++ -graveyard-shift/scripts/maze_room.gd
@@ -8,5 +8,6 @@ var inside_maze : bool = false
 func _on_maze_trigger_body_entered(body: Node3D) -> void:
 	if not inside_maze:
 		if body is CharacterBody3D:
+			GameManager.set_phase(GameManager.Phase.FINAL)
 			willie.change_state("maze")
 			inside_maze = true
